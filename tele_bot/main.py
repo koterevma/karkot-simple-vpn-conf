cbd5a6c2f8a740331565733ec97942152b0fef8c
Maksim Koterev <makoterev@gmail.com>
2022-07-11 00:05:21 +0300

    Unfinished bot in docker

--- tele_bot/main.py
+++ tele_bot/main.py
@@ -3,16 +3,18 @@
 """
 Bot to give access to vpn configuration
 
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+Ships in docker container
 """
 
 import logging
 import os
 import userdata
+import wgconf
 
-from telegram import ForceReply, Update
+from telegram import ForceReply, Update, Bot
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.constants import ParseMode
+
 
 # Enable logging
 logging.basicConfig(
@@ -21,19 +23,44 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+async def send_confirmation_request(bot: Bot, username: str, user_id: int) -> None:
+    admin_ids = userdata.get_admin_ids()
+    for admin_id in admin_ids:
+        await bot.send_message(
+                admin_id, rf"@{username} requests config\. `/accept {user_id}`",
+                parse_mode=ParseMode.MARKDOWN_V2)
+
+
+# Start command - register user and send confirmation request to admins
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
     data_path, is_admin = userdata.get_user_data(user.id)
-    text = f"{user.id=}, {data_path=}, {is_admin=}"
+    text = "User already registred, waiting approval from admins"
+    if data_path is not None:
+        text = f"User already exists, type /get_config to get configuration"
+    if data_path is None and is_admin is None:
+        userdata.add_user(user.id, None, 0)
+        await send_confirmation_request(context.bot, user.username, user.id)
+        text = "Registred new user and send confirmation message to admins"
     await update.message.reply_text(text)
 
 
 async def help_(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Type /start to send a request for a vpn configuration")
+
+
+async def accept(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Accept user and give them configuration"""
+    accepted_user_id = update.message.text.split()[1]
+    new_config_dir = wgconf.get_new_config()
+    userdata.update_user(accepted_user_id, str(new_config_dir))
+    bot: Bot = context.bot
+    await bot.send_photo(accepted_user_id,
+            "Request accepted! Here's your configuration",
+            filename=new_config_dir / (new_config_dir.name + ".png"))
+    await bot.send_document(accepted_user_id, new_config_dir / (new_config_dir.name + ".conf"))
 
 
 def main() -> None:
@@ -52,6 +79,7 @@ def main() -> None:
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_))
+    application.add_handler(CommandHandler("accept", accept))
 
     # on non command i.e message - print help
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, help_))
--- tele_bot/userdata.py
+++ tele_bot/userdata.py
@@ -11,20 +11,33 @@ USERDATA_DB = Path("./data/userdata.db")
 CREATE_TABLE = (
         "CREATE TABLE IF NOT EXISTS user("
         "id INTEGER UNIQUE PRIMARY KEY NOT NULL, "
-        "conf_file TEXT, "
+        "data_path TEXT, "
         "is_admin INTEGER"
         ");"
 )
 
 ADD_USER = (
-        "INSERT INTO user (id, conf_file, is_admin)"
+        "INSERT INTO user (id, data_path, is_admin)"
         "VALUES(?, ?, ?);"
 )
 
 GET_USER_DATA = (
-        "SELECT conf_file, is_admin "
+        "SELECT data_path, is_admin "
         "FROM user "
-        "WHERE id == ?;"
+        "WHERE id = ?;"
+)
+
+UPDATE_USER = (
+        "UPDATE user "
+        "SET data_path = ? "
+        "is_admin = ? "
+        "WHERE id = ?;"
+)
+
+GET_ADMIN_IDS = (
+        "SELECT id "
+        "FROM user "
+        "WHERE is_admin = 1;"
 )
 
 
@@ -33,23 +46,46 @@ with sqlite3.connect(USERDATA_DB) as conn:
     conn.commit()
 
 
-def add_admins(admins: str):
+def add_user(user_id: int, data_path: str, is_admin: int) -> None:
+    with sqlite3.connect(USERDATA_DB) as conn:
+        cur = conn.cursor()
+        try:
+            cur.execute(ADD_USER, (user_id, data_path, is_admin))
+            conn.commit()
+        except sqlite3.IntegrityError:
+            logger.warn(f"User {user_id} not added, already exists in database")
+    
+
+def add_admins(admins: str) -> None:
     admins_data = [(admin_id, None, 1) for admin_id in admins.split(",")]
-    with sqlite3.connect(USERDATA_DB) as conn:
-        cur = conn.cursor()
-        for admin_data in admins_data:
-            try:
-                cur.execute(ADD_USER, admin_data)
-                conn.commit()
-            except sqlite3.IntegrityError:
-                logger.warn(f"Admin {admin_data[0]} not added, already exists")
+    for admin_data in admins_data:
+        add_user(*admin_data)
 
 
-def get_user_data(id: int) -> tuple[str, int]:
+def update_user(user_id: int, data_path: str, is_admin: int | None = None):
+    if is_admin is None:
+        is_admin = get_user_data(user_id)[1]
     with sqlite3.connect(USERDATA_DB) as conn:
         cur = conn.cursor()
-        result = cur.execute(GET_USER_DATA, (id,)).fetchone()
+        try:
+            cur.execute(UPDATE_USER, (data_path, is_admin, user_id))
+            conn.commit()
+        except sqlite3.Error as e:
+            logger.error("In update_user: " + str(e))
+
+
+def get_user_data(id_: int) -> tuple[str, int]:
+    with sqlite3.connect(USERDATA_DB) as conn:
+        cur = conn.cursor()
+        result = cur.execute(GET_USER_DATA, (id_,)).fetchone()
         if result == None:
             return (None, None)
         return result
+
+
+def get_admin_ids() -> list[int]:
+    with sqlite3.connect(USERDATA_DB) as conn:
+        cur = conn.cursor()
+        result = cur.execute(GET_ADMIN_IDS).fetchall()
+        return [i[0] for i in result]
 
